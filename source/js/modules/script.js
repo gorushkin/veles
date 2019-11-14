a24d54166c59d6a6e1fff1d486ea99aff20d37be
gorushkin <gorushkin@gmail.com>
2019-11-14 23:27:35 +0300

    first commit

---
+++ source/js/modules/script.js
@@ -0,0 +1,54 @@
+'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+
+// pageHeader.classList.remove('page-header--nojs');
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+console.log('test!!!');
+
+(function() {
+  // var mySwiper = new Swiper ('.swiper-container', {
+  //   // Optional parameters
+  //   // direction: 'vertical',
+  //   loop: true,
+
+  //   // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'fraction',
+  //   },
+
+  //   // Navigation arrows
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+
+  //   // And if we need scrollbar
+  //   scrollbar: {
+  //     // el: '.swiper-scrollbar',
+  //   },
+  // })
+
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+})();
