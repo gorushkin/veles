b1d6cd82c807c6b87d80e026d8474f05b5ccb195
gorushkin <gorushkin@gmail.com>
2019-11-15 10:11:09 +0300

    настраивает слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -17,29 +17,6 @@
 console.log('test!!!');
 
 (function() {
-  // var mySwiper = new Swiper ('.swiper-container', {
-  //   // Optional parameters
-  //   // direction: 'vertical',
-  //   loop: true,
-
-  //   // If we need pagination
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'fraction',
-  //   },
-
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-
-  //   // And if we need scrollbar
-  //   scrollbar: {
-  //     // el: '.swiper-scrollbar',
-  //   },
-  // })
-
   var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
